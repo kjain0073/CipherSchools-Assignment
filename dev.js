07c20e447d494b86d44851917c9a3cff251afd29
kjain0073 <kjain20040073@gmail.com>
2020-08-10 02:48:14 +0530

    improvements added

--- dev.js
+++ dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://testuser:test@cluster0.yngtu.mongodb.net/mernloginreg?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://testuser1:test1@cluster0.yngtu.mongodb.net/mernloginreg?retryWrites=true&w=majority'
 }
